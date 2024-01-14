78606ed4e974f0db2d36182f21a5d5923ce31909
Rishika <en20cs303041@medicaps.ac.in>
2024-01-14 12:20:37 +0530

    Adding Discount model and role API

--- server/models/userModel/userModel.js
+++ server/models/userModel/userModel.js
@@ -23,5 +23,5 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const UserModel = mongoose.model("user", UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 module.exports = UserModel;
