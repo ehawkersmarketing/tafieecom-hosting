fcec0eaaaf2fe46a5f257c5d3e94792cfc730896
bhavik <kotharibhavik23@gmail.com>
2024-01-16 14:16:17 +0530

    changes in image uploading and made search API for products

--- client/src/app/pages/create_product/createProduct.js
+++ client/src/app/pages/create_product/createProduct.js
@@ -1,140 +1,135 @@
-import react ,{useState} from 'react';
-import { useNavigate  } from 'react-router-dom';
-import './createProduct.css'
+import react, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './createProduct.css';
+import axios from 'axios';
+const CreateProduct = () => {
+  const [image, setImage] = useState();
+  const user = JSON.parse(localStorage.getItem("user"));
 
-const CreateProduct = () =>{
+  const [inputHandler, setInputHandler] = useState({
+    title: '',
+    description: '', price: '', quantity: '', productType: ''
+  });
 
-  const user =  JSON.parse(localStorage.getItem("user"));
+  const history = useNavigate()
 
-    const [inputHandler, setInputHandler] = useState({title:'',
-        description:'' , price:'' , image:'' , quantity:'', productType:''
-      });
+  const onChangeInputHandler = (e) => {
+    const { name, value } = e.target;
+    setInputHandler(() => {
+      return { ...inputHandler, [name]: value };
+    });
 
-      const history = useNavigate() 
-    
-      const onChangeInputHandler = (e) => {
-        const { name, value } = e.target; 
-        setInputHandler(() => {
-          return { ...inputHandler, [name]: value };
-        });
+  };
 
-      };
+  // console.log(inputHandler)
 
-      // console.log(inputHandler)
-    
-      const onSubmitHandler = async (e) => {
-        e.preventDefault();
-        
-        const { title , description ,image,price ,quantity, productType } = inputHandler;
-        console.log(inputHandler)
-        //  console.log(image)
-        if (title === "") {
-          alert("Add Title"); 
-        } else if (description === "") {
-          alert(" Add Description");
-        } else if (quantity === "") {
-            alert("Add Quantity");
-          } else if (price === "") {
-            alert("Price");
-          } 
-         else {
-          const data = await fetch("http://localhost:8080/api/createProduct", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body:JSON.stringify({
-              title , description ,image, price , quantity, productType  
-            })
-          });
-   
-          const res =await  data.json()
-              console.log(res)
-          if(res.status === 200){
-            setInputHandler({...inputHandler , title:" ",image:" ", description:" " , price:'' , quantity:'', productType:'' })
-                history('/')
-          }
-        }
-      };
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const { title, description, price, quantity, productType } = inputHandler;
+    console.log(inputHandler)
+    if (title === "") {
+      alert("Add Title");
+    } else if (description === "") {
+      alert(" Add Description");
+    } else if (quantity === "") {
+      alert("Add Quantity");
+    } else if (price === "") {
+      alert("Price");
+    }
+    else {
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("description", description);
+      formData.append("price", price);
+      formData.append("quantity", quantity);
+      formData.append("productType", productType);
+      formData.append("image", image);
+      console.log(image);
+      formData.append("fileName", image.name);
+      const { data } = await axios.post('http://localhost:8080/api/createProduct', formData);
+      if (data.success) {
+        setInputHandler({ ...inputHandler, title: " ", description: " ", price: '', quantity: '', productType: '' })
+        history('/')
+      }
+    }
+  };
 
   return (
     <section>
-        {  <div className="form_data">
-          <div className="form_heading">
-            <h1>Create Product</h1>
+      {<div className="form_data">
+        <div className="form_heading">
+          <h1>Create Product</h1>
+        </div>
+
+        <form>
+          <div className="form_input">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              onChange={onChangeInputHandler}
+              value={inputHandler.title}
+              id="title"
+              name="title"
+              placeholder="Title"
+            />
+          </div>
+          <div className="form_input">
+            <label htmlFor="Description">Description</label>
+            <input
+              type="text"
+              onChange={onChangeInputHandler}
+              value={inputHandler.description}
+              id="description"
+              name="description"
+              placeholder="Description"
+            />
+          </div>
+          <div className="form_input">
+            <label for="productImage">Product Image</label>
+
+            <input type="file" id="productImage" onChange={e => setImage(e.target.files[0])}
+              name="productImage" required />
           </div>
 
-          <form>
-            <div className="form_input">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                onChange={onChangeInputHandler}
-                value={inputHandler.title}
-                id="title"
-                name="title"
-                placeholder="Title"
-              />
-            </div>
-            <div className="form_input">
-              <label htmlFor="Description">Description</label>
-              <input
-                type="text"
-                onChange={onChangeInputHandler}
-                value={inputHandler.description}
-                id="description"
-                name="description"
-                placeholder="Description"
-              />
-            </div>
-            <div className="form_input">
-            <label for="productImage">Product Image</label>
-        <input type="file" id="productImage" onChange={(e) =>{
-          console.log(e.target.files);
-          // setInputHandler({...inputHandler, ['image']:e.target.files[0] });
-        }}
-                 name="productImage" value={inputHandler.image} required />
-</div>
-
-            <div className="form_input">
-              <label htmlFor="price">Price</label>
-              <input
-                type="text"
-                onChange={onChangeInputHandler}
-                value={inputHandler.price}
-                id="price"
-                name="price"
-                placeholder="Price"
-              />
-            </div>
-            <div className="form_input">
-              <label htmlFor="quantity">Quantity</label>
-              <input
-                type="text"
-                onChange={onChangeInputHandler}
-                value={inputHandler.quantity}
-                id="quantity"
-                name="quantity"
-                placeholder="quantity"
-              />
-            </div>
-            <div className="form_input">
-              <label htmlFor="productType">Product Type</label>
-              <input
-                type="text"
-                onChange={onChangeInputHandler}
-                value={inputHandler.productType}
-                id="productType"
-                name="productType"
-                placeholder="productType"
-              />
-            </div>
-            <button className="btn" onClick={onSubmitHandler}>
-              Create Product
-            </button>
-          </form>
-        </div>}
-      </section>
+          <div className="form_input">
+            <label htmlFor="price">Price</label>
+            <input
+              type="text"
+              onChange={onChangeInputHandler}
+              value={inputHandler.price}
+              id="price"
+              name="price"
+              placeholder="Price"
+            />
+          </div>
+          <div className="form_input">
+            <label htmlFor="quantity">Quantity</label>
+            <input
+              type="text"
+              onChange={onChangeInputHandler}
+              value={inputHandler.quantity}
+              id="quantity"
+              name="quantity"
+              placeholder="quantity"
+            />
+          </div>
+          <div className="form_input">
+            <label htmlFor="productType">Product Type</label>
+            <input
+              type="text"
+              onChange={onChangeInputHandler}
+              value={inputHandler.productType}
+              id="productType"
+              name="productType"
+              placeholder="productType"
+            />
+          </div>
+          <button className="btn" onClick={onSubmitHandler}>
+            Create Product
+          </button>
+        </form>
+      </div>}
+    </section>
   )
 }
 
--- server/routes/productRoute/productRoute.js
+++ server/routes/productRoute/productRoute.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const {
-  getAllProducts, createProduct , updateProduct , deleteProduct
+  getAllProducts, createProduct, updateProduct, deleteProduct, searchProduct
 } = require("../../controlllers/product/productController");
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 // GET || getting all blogs
-router.post("/createProduct",upload.single('image'),createProduct);
+router.post("/createProduct", upload.single('image'), createProduct);
 router.get("/allProducts", getAllProducts);
 router.patch("/updateProduct/:id", updateProduct);
 router.delete("/deleteProduct/:id", deleteProduct);
+router.post("/searchProduct", searchProduct);
 
 module.exports = router;
