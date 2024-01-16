3d695beef9f7a05b1f990f3809dbbd88e309e5c0
bhavik <kotharibhavik23@gmail.com>
2024-01-16 19:06:57 +0530

    sdgs

--- server/routes/cartRoute/cartRoute.js
+++ server/routes/cartRoute/cartRoute.js
@@ -2,8 +2,6 @@ const express = require("express");
 const {
   putProductInCart,
   deleteProductInCart,
-  editProductUnits,
-  getCart,
 } = require("../../controlllers/cart/cartController");
 
 const router = express.Router();
@@ -13,11 +11,5 @@ router.put("/addToCart", putProductInCart);
 
 //DELETE || delete product route
 router.delete("/dropFromCart", deleteProductInCart);
-
-//PUT || change units of a product
-router.put("/changeUnits", editProductUnits);
-
-//GET || visit the cart page
-router.get("/cart", getCart);
 
 module.exports = router;
