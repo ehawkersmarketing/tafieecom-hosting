8ef3b57d9c2a35eb8291b8268f7f63db3c37d2ee
Rishika <en20cs303041@medicaps.ac.in>
2024-01-20 18:04:05 +0530

    home page

--- client/src/app/pages/home_page/dashboard.jsx
+++ client/src/app/pages/home_page/dashboard.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import ProductCard from '../../components/product_card/product_card.jsx';
-import Header from '../header/header.jsx';
+import ProductCard from "../../components/product_card/product_card.jsx";
+import Header from "../header/header.jsx";
 import Footer from "../footer/footer.jsx";
-import punchline from '../../assets/punchline.png';
+import punchline from "../../assets/punchline.png";
 import { ReactSVG } from "react-svg";
-
-
-import './home_page.css';
-
+import "./home_page.css";
+import CategoryCarousel from "../../pages/shop_page/component/categoryCarousel/categoryCarousel.jsx";
 const Dashboard = () => {
   return (
     <>
       <Header />
-      <div>
-        <div className="topDiv">
-          <div className="headingDiv">
+      <div className="home-page">
+        <div className="topDiv row">
+          <div className="headingDiv col-5">
             <div className="headingMainText">
-              <h1>(UNNAT KHETI,</h1>
-              <h1>UNNAT KISAN)</h1>
+              <h1>उच्च खेती,</h1>
+              <h1>उन्नत विकास</h1>
               <img className="punchImg" src={punchline} />
             </div>
             <div className="iconDiv">
@@ -38,13 +36,61 @@ const Dashboard = () => {
               <button>Shop Now</button>
             </div>
           </div>
+          <div className="headingimage col-7"></div>
         </div>
-        <div className="categoryDiv">
-          <h1>Product Categories</h1>
+        <div className="categoryDiv ">
+          <div className="product-category">
+            <div className="product-category-text">
+              <div className="category-text">
+                <div className="tafi-product-text1">
+                  <span>Product</span>
+                </div>
+                <div className="tafi-product-text2">
+                  <span>Categories</span>
+                </div>
+              </div>
+            </div>
+            <div className="category-region">
+              <div className="category-carousel">
+                <div className="left-arrow">
+                  <i class="bi bi-arrow-left"></i>
+                </div>
+                <CategoryCarousel />
+                <div className="right-arrow">
+                  <i class="bi bi-arrow-right"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="featured-product">
+          <div className="product-category-text">
+            <div className="category-text">
+              <div className="tafi-product-text1">
+                <span>Featured</span>
+              </div>
+              <div className="tafi-product-text2">
+                <span>Products</span>
+              </div>
+            </div>
+          </div>
+          <div className="category-region">
+            <div className="category-carousel">
+              <div className="left-arrow">
+                <i class="bi bi-arrow-left"></i>
+              </div>
+              <CategoryCarousel />
+              <div className="right-arrow">
+                <i class="bi bi-arrow-right"></i>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="div3">
           <div className="div3Heading">
-            <h1>EMPOWER</h1>
+            <div>
+              <h1>EMPOWER</h1>
+            </div>
             <div className="div3SubHead">
               <h4>YOUR</h4>
               <h4 style={{ color: "#44A98B" }}>FARMING</h4>
@@ -52,64 +98,82 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="servicesDiv">
-            <div className="service1Div singleServiceDiv">
-              <div className="serviceCircle">
-                <div className="tick-icon">
-                  <i className="bi bi-patch-check-fill serviceIcon"></i>
+            <div className="service100">
+              <div className="singleServiceDiv">
+                <div className="serviceCircle">
+                  <div className="tick-icon">
+                    <i className="bi bi-patch-check-fill serviceIcon"></i>
+                  </div>
                 </div>
+                <h5>Exceptional Product Quality</h5>
               </div>
-              <h5>Exceptional Product Quality</h5>
-
             </div>
-            <div className="service2Div singleServiceDiv">
-              <div className="serviceCircle">
-                <i className="bi bi-key-fill serviceIcon"></i>
+            <div className="service100">
+              <div className="singleServiceDiv">
+                <div className="serviceCircle">
+                  <i className="bi bi-key-fill serviceIcon"></i>
+                </div>
+                <h5>Ease of Access</h5>
               </div>
-              <h5>Ease of Access</h5>
-
             </div>
-            <div className="service3Div singleServiceDiv">
-              <div className="serviceCircle">
-                <i className="bi bi-headset serviceIcon"></i>
+            <div className="service100">
+              <div className="singleServiceDiv">
+                <div className="serviceCircle">
+                  <i className="bi bi-headset serviceIcon"></i>
+                </div>
+                <h5>Expert Advice & Support</h5>
               </div>
-              <h5>Expert Advice & Support</h5>
-
             </div>
-            <div className="service4Div singleServiceDiv">
-              <div className="serviceCircle">
-                <i className="bi bi-wallet2 serviceIcon" ></i>
+            <div className="service100">
+              <div className="singleServiceDiv">
+                <div className="serviceCircle">
+                  <i className="bi bi-wallet2 serviceIcon"></i>
+                </div>
+                <h5>Excellent Cost Efficiency</h5>
               </div>
-              <h5>Excellent Cost Efficiency</h5>
             </div>
           </div>
         </div>
-        <div className="div4">
-          <div className="soilDiv d-flex justify-content-end align-items-end">
+        <div className="div4 row ">
+          <div className="soilDiv col-5">
             <div className="soilTestInfo">
               <div className="soilHeading">
                 <h1>FREE</h1>
                 <h5 className="soilSubHead1">SOIL TESTING</h5>
-                <h5 className="soilSubHead2">(Know Your Soil, Grow Better Crops!)</h5>
+                <h5 className="soilSubHead2">
+                  (Know Your Soil, Grow Better Crops!)
+                </h5>
               </div>
-              <p>Discover the decret to successfull farming, sign up for free soil testing.</p>
+              <div className="soliDescription">
+                <p>
+                  Discover the decret to successfull farming, sign up for free
+                  soil testing.
+                </p>
+              </div>
               <div className="registerSoilBTN">
-                <button>Register Now<i class="bi bi-chevron-right rigthIcon"></i></button>
+                <button>
+                  Register Now<i class="bi bi-chevron-right rigthIcon"></i>
+                </button>
               </div>
             </div>
           </div>
-          <div className="franDiv">
+          <div className="franDiv col-7">
             <div className="franchiseInfo">
               <div className="franchHeading">
                 <h1>70+</h1>
                 <h3>FRANCHISES OVER INDIA</h3>
                 <div className="franchSubHead">
-                  <h6>Ready  to  take  your  farming  journey  to  the </h6>
-                  <h6 className="franchSubHead3">Next  Level?</h6>
+                  <h6>Ready to take your farming journey to the </h6>
+                  <h6 className="franchSubHead3">Next Level?</h6>
                 </div>
               </div>
             </div>
             <div className="franInfoDiv2">
-              <p><strong>Join the Krishak Vatika</strong> family today and harvest the success tomorrow!</p><div className="registerSoilBTN">
+              <p>
+                <strong>Join the Krishak Vatika</strong> family today and
+                harvest the success tomorrow!
+              </p>
+              <div className="registerSoilBTN">
                 <button>Know More</button>
               </div>
             </div>
@@ -119,7 +183,6 @@ const Dashboard = () => {
       <Footer />
     </>
   );
-}
+};
 
 export default Dashboard;
-
