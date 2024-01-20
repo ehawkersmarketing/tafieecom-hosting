e7317ccecde2a472363fbde4ae26b7934203ab10
bhavik <kotharibhavik23@gmail.com>
2024-01-20 13:03:41 +0530

    removing conflicts

--- server/models/orderModel/orderModel.js
+++ server/models/orderModel/orderModel.js
@@ -25,6 +25,10 @@ const orderSchema = new mongoose.Schema(
             type: String,
             default: "PROCESSING",
         },
+        orderStatus: {
+            type: String,
+            default: "PROCESSING",
+        },
         amount: {
             type: Number,
         },
