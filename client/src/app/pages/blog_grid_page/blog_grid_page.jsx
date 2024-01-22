d0ab7ae4975186a812e78bd4c349c0f36d48a1b2
ayushi yadav <ayushiyadav.bce@gmail.com>
2024-01-22 17:56:42 +0530

    franchise page

--- client/src/app/pages/blog_grid_page/blog_grid_page.jsx
+++ client/src/app/pages/blog_grid_page/blog_grid_page.jsx
@@ -5,19 +5,23 @@ import { useFetch } from "../../hooks/api_hook";
 import dayjs from "dayjs";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Header from "../../pages/header/header";
+import Footer from "../../pages/footer/footer";
 
 const BlogPage = () => {
-
-  const { data: blogs } = useFetch('/api/blogs');
-  const [searchField, setSearchField] = useState('');
-  const { data: recentBlogs } = useFetch('/api/recentBlogs');
+  const { data: blogs } = useFetch("/api/blogs");
+  const [searchField, setSearchField] = useState("");
+  const { data: recentBlogs } = useFetch("/api/recentBlogs");
   const [searchBlogs, setSearchBlog] = useState([]);
 
   const search = async (text) => {
-    if (text !== '') {
-      const { data } = await axios.post(`http://localhost:8080/api/searchBlog`, {
-        search: text
-      });
+    if (text !== "") {
+      const { data } = await axios.post(
+        `http://localhost:8080/api/searchBlog`,
+        {
+          search: text,
+        }
+      );
       setSearchBlog(data.data);
     } else {
       setSearchBlog(undefined);
@@ -30,6 +34,7 @@ const BlogPage = () => {
 
   return (
     <div>
+      <Header />
       <div className="blogpage">
         <div className="blog-tile">
           <div className="blog-grid-page row">
@@ -40,10 +45,14 @@ const BlogPage = () => {
                   <h3>Welcome to</h3>
                 </div>
                 <div className="tafi-resource">
-                  <h1><span>TAFI RESOURCE</span></h1>
+                  <h1>
+                    <span>TAFI RESOURCE</span>
+                  </h1>
                 </div>
                 <div className="center">
-                  <h1><span>CENTER</span></h1>
+                  <h1>
+                    <span>CENTER</span>
+                  </h1>
                 </div>
               </div>
             </div>
@@ -61,7 +70,12 @@ const BlogPage = () => {
               <i class="bi bi-caret-down-fill"></i>
             </div>
             <div className="search-bar">
-              <input type="text" name="search" onChange={(e) => setSearchField(e.target.value)} className="search_container" />
+              <input
+                type="text"
+                name="search"
+                onChange={(e) => setSearchField(e.target.value)}
+                className="search_container"
+              />
               <div className="search-button">
                 <button className="search-icon">
                   <i class="bi bi-search"></i>
@@ -70,8 +84,7 @@ const BlogPage = () => {
             </div>
           </div>
         </div>
-        {
-          searchField !== '' &&
+        {searchField !== "" && (
           <div className="blog-latest-post">
             <div>
               <h4>Search Posts</h4>
@@ -81,30 +94,41 @@ const BlogPage = () => {
               <div className="below-post-1"></div>
             </div>
             <div className="latest-post-card row">
-              {
-                searchBlogs && searchBlogs.length !== 0 ? searchBlogs?.map((blog) => {
-                  return <div className="card-main col-md-4">
-                    <div class="card">
-                      <img src={resourcepage1} class="card-img-top" alt="..." />
-                      <div class="card-body">
-                        <h5 class="card-title">
-                          {blog.title}
-                        </h5>
-                        <p class="card-text">
-                          {blog.content}
-                        </p>
-                        <p class="blog-date">{`${dayjs(blog.createdAt).format('MMMM D, YYYY')}`}</p>
-                        <Link to={`/singleBlog/${blog._id}`} class="btn btn-read">
-                          Read More
-                        </Link>
+              {searchBlogs && searchBlogs.length !== 0 ? (
+                searchBlogs?.map((blog) => {
+                  return (
+                    <div className="card-main col-md-4">
+                      <div class="card">
+                        <img
+                          src={resourcepage1}
+                          class="card-img-top"
+                          alt="..."
+                        />
+                        <div class="card-body">
+                          <h5 class="card-title">{blog.title}</h5>
+                          <p class="card-text">{blog.content}</p>
+                          <p class="blog-date">{`${dayjs(blog.createdAt).format(
+                            "MMMM D, YYYY"
+                          )}`}</p>
+                          <Link
+                            to={`/singleBlog/${blog._id}`}
+                            class="btn btn-read"
+                          >
+                            Read More
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                }) : <div><h4>No Results Found</h4></div>
-              }
+                  );
+                })
+              ) : (
+                <div>
+                  <h4>No Results Found</h4>
+                </div>
+              )}
             </div>
           </div>
-        }
+        )}
         <div className="blog-latest-post">
           <div>
             <h4>Latest Posts</h4>
@@ -114,27 +138,29 @@ const BlogPage = () => {
             <div className="below-post-1"></div>
           </div>
           <div className="latest-post-card row">
-            {
-              recentBlogs && recentBlogs?.map((blog) => {
-                return <div className="card-main col-md-4">
-                  <div class="card">
-                    <img src={resourcepage1} class="card-img-top" alt="..." />
-                    <div class="card-body">
-                      <h5 class="card-title">
-                        {blog.title}
-                      </h5>
-                      <p class="card-text">
-                        {blog.content}
-                      </p>
-                      <p class="blog-date">{`${dayjs(blog.createdAt).format('MMMM D, YYYY')}`}</p>
-                      <Link to={`/singleBlog/${blog._id}`} class="btn btn-read">
-                        Read More
-                      </Link>
+            {recentBlogs &&
+              recentBlogs?.map((blog) => {
+                return (
+                  <div className="card-main col-md-4">
+                    <div class="card">
+                      <img src={resourcepage1} class="card-img-top" alt="..." />
+                      <div class="card-body">
+                        <h5 class="card-title">{blog.title}</h5>
+                        <p class="card-text">{blog.content}</p>
+                        <p class="blog-date">{`${dayjs(blog.createdAt).format(
+                          "MMMM D, YYYY"
+                        )}`}</p>
+                        <Link
+                          to={`/singleBlog/${blog._id}`}
+                          class="btn btn-read"
+                        >
+                          Read More
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              })
-            }
+                );
+              })}
           </div>
         </div>
         <div className="blog-most-viewed">
@@ -147,27 +173,33 @@ const BlogPage = () => {
           </div>
           <div className="most-viewed-cards">
             <div className="most-viewed-post-card row">
-              {
-                blogs && blogs.slice(0, 3)?.map((blog) => {
-                  return <div className="card-main col-md-4">
-                    <div class="card">
-                      <img src={resourcepage1} class="card-img-top" alt="..." />
-                      <div class="card-body">
-                        <h5 class="card-title">
-                          {blog.title}
-                        </h5>
-                        <p class="card-text">
-                          {blog.content}
-                        </p>
-                        <p class="blog-date">{`${dayjs(blog.createdAt).format('MMMM D, YYYY')}`}</p>
-                        <Link to={`/singleBlog/${blog._id}`} class="btn btn-read">
-                          Read More
-                        </Link>
+              {blogs &&
+                blogs.slice(0, 3)?.map((blog) => {
+                  return (
+                    <div className="card-main col-md-4">
+                      <div class="card">
+                        <img
+                          src={resourcepage1}
+                          class="card-img-top"
+                          alt="..."
+                        />
+                        <div class="card-body">
+                          <h5 class="card-title">{blog.title}</h5>
+                          <p class="card-text">{blog.content}</p>
+                          <p class="blog-date">{`${dayjs(blog.createdAt).format(
+                            "MMMM D, YYYY"
+                          )}`}</p>
+                          <Link
+                            to={`/singleBlog/${blog._id}`}
+                            class="btn btn-read"
+                          >
+                            Read More
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                })
-              }
+                  );
+                })}
             </div>
           </div>
         </div>
@@ -180,35 +212,38 @@ const BlogPage = () => {
             <div className="below-post-1"></div>
           </div>
           <div className="all-post-card row ">
-            {
-              blogs && blogs?.map((blog) => {
-                return <div className="card-main col-md-4">
-                  <div class="card">
-                    <img src={resourcepage1} class="card-img-top" alt="..." />
-                    <div class="card-body">
-                      <h5 class="card-title">
-                        {blog.title}
-                      </h5>
-                      <p class="card-text">
-                        {blog.content}
-                      </p>
-                      <p class="blog-date">{`${dayjs(blog.createdAt).format('MMMM D, YYYY')}`}</p>
-                      <Link to={`/singleBlog/${blog._id}`} class="btn btn-read">
-                        Read More
-                      </Link>
+            {blogs &&
+              blogs?.map((blog) => {
+                return (
+                  <div className="card-main col-md-4">
+                    <div class="card">
+                      <img src={resourcepage1} class="card-img-top" alt="..." />
+                      <div class="card-body">
+                        <h5 class="card-title">{blog.title}</h5>
+                        <p class="card-text">{blog.content}</p>
+                        <p class="blog-date">{`${dayjs(blog.createdAt).format(
+                          "MMMM D, YYYY"
+                        )}`}</p>
+                        <Link
+                          to={`/singleBlog/${blog._id}`}
+                          class="btn btn-read"
+                        >
+                          Read More
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              })
-            }
+                );
+              })}
           </div>
-          <div className="view-more">
+          {/* <div className="view-more">
             <button class="btn btn-read " id="show-more">
               Show More
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
