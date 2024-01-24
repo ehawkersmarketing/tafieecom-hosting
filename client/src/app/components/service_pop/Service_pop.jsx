927436178db042de86d3d4cf6f7e608a5c02a461
Rishika <en20cs303041@medicaps.ac.in>
2024-01-24 16:14:09 +0530

    service page

--- client/src/app/components/service_pop/Service_pop.jsx
+++ client/src/app/components/service_pop/Service_pop.jsx
@@ -5,20 +5,18 @@ import './Service_pop.css'
 
 const Service_pop = () => {
     return(
-        <div className="service-pop-main">
+        <div className="service-pop-main" id="servicemodal">
             <div className="service-pop-submain row">
-                <div className="service-pop-image col-5">
+                <div className="service-pop-image col-6">
                     <img src={PopUp} />
                 </div>
-                <div className="service-pop-text col-5">
+                <div className="service-pop-text col-6">
                     <div className="service-pop-heading-text">POLY HOUSE</div>
                         <div className="service-pop-subtext"><p>Environment-friendly structure made of 
                             polythene sheet for the cultivation of high quality and exotic crops. 
                             Which creates high quality and high yield by creating crop-friendly behavior.</p>
                         </div>
-                        <div className='service-pop'>
-                            <button className="d-flex" type="link"> <div><strong>Show Interest</strong></div><i class="bi bi-whatsapp"></i></button>
-                        </div>
+                        
                 </div>   
             </div>
         </div>
